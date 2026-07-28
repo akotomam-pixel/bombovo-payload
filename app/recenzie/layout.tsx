0cad702c0d4bcd0aee3237758596a9eb57fe12a3
Matej <matej@Matejs-MacBook-Pro.local>
2026-07-28 17:40:50 +0200

    Remove password gate on /recenzie

--- app/recenzie/layout.tsx
+++ app/recenzie/layout.tsx
@@ -1,6 +1,5 @@
 import '../(app)/globals.css'
-import PasswordGate from './PasswordGate'
 
 export default function RecenzieLayout({ children }: { children: React.ReactNode }) {
-  return <PasswordGate>{children}</PasswordGate>
+  return <>{children}</>
 }
